ddc1d139346599308eccea8036ba9794eee68c16
EunSeo <144340396+pdmstj@users.noreply.github.com>
2025-01-29 02:51:33 +0900

    [Bronze V] Title: A+B - 8, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+if let testCaseCount = Int(readLine()!) {
+    for i in 1...testCaseCount {
+        if let input = readLine() {
+            let numbers = input.split(separator: " ").compactMap { Int($0) }
+            if numbers.count == 2 {
+                let a = numbers[0]
+                let b = numbers[1]
+                print("Case #\(i): \(a) + \(b) = \(a + b)")
+            }
+        }
+    }
+}
