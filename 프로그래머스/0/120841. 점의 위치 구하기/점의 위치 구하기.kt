3eff6f2d5af9b0f8342b93c494e78888b3f7ee41
EunSeo <144340396+pdmstj@users.noreply.github.com>
2025-03-29 22:28:33 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 63.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(dot: IntArray): Int {
+        return when {
+            dot[0] > 0 && dot[1] > 0 -> 1
+            dot[0] < 0 && dot[1] > 0 -> 2
+            dot[0] < 0 && dot[1] < 0 -> 3
+            else -> 4
+        }
+    }
+}
